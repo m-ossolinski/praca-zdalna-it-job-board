4fb042f3377d3449c6c4a0729c0cdbca192c8e9e
m-ossolinski <mateusz.ossolinski@gmail.com>
2020-04-30 18:41:25 +0200

    added proxy between client and server app

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -1,5 +1,11 @@
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.send('default route')
+  app.get('/api/test', (req, res) => {
+    const testResponse = [
+      { id: 1, name: 'john', lastName: 'smith' },
+      { id: 2, name: 'john', lastName: 'smith' },
+      { id: 3, name: 'john', lastName: 'smith' },
+    ];
+
+    res.json(testResponse);
   });
 }
