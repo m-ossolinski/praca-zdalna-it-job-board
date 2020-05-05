d9101e36189dd634c1d7da95307169559191ea1c
m-ossolinski <mateusz.ossolinski@gmail.com>
2020-05-05 15:43:55 +0200

    added ability to login user via google oauth, logout and check for current user logged in app in the server side

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const authRoutes = require('./routes/authenticationRoutes');
-const { mongoURI } = require('./config/keys');
+const { mongoURI, cookieKey } = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
--- services/passport.js
+++ services/passport.js
@@ -5,11 +5,31 @@ const { googleClientID, googleClientSecret } = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user));
+})
+
 passport.use(new GoogleStrategy({
   clientID: googleClientID,
   clientSecret: googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true,
 }, (accessToken, refreshToken, profile, done) => {
-  new User({ googleId: profile.id }).save();
+  User.findOne({ googleId: profile.id })
+    .then(existingUser => {
+      console.log(existingUser);
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
+      }
+    })
+    .catch(err => console.log(err));
 }));
