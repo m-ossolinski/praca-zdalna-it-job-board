0e16ae6602aaf8ed5f3811e08d56ee25cc4fabd7
m-ossolinski <mateusz.ossolinski@gmail.com>
2020-05-05 16:56:04 +0200

    added basic version for loggin/logout via google api

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,58 @@
 import React  from 'react';
+import { connect } from 'react-redux';
+import * as actions from './actions';
 
-function App() {
-  return (
-    <div className="App">
-      praca-zdalna.it - job board
-      <a
-        href="/auth/google"
-        style={{ background: 'red', padding: '10px' }}
-      >
-        login
-      </a>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">
+              Login with google
+            </a
+            ></li>
+        );
+
+      default:
+        return (
+          <>
+            <li>
+              payments
+            </li>
+            <li style={{ margin: '0 10px' }}>
+              Credits: 1
+            </li>
+            <li>
+              <a href="/api/logout">
+                Logout
+              </a
+              >
+            </li>
+          </>
+        );
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.renderContent()}
+        praca-zdalna.it - job board
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(reduxThunk)));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
