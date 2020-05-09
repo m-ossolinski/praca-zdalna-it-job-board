9f3893b25604a606899fb3b808127326ef118dcb
m-ossolinski <mateusz.ossolinski@gmail.com>
2020-05-09 15:33:57 +0200

    installed body-parser lib

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authenticationRoutes');
 const offerRoutes = require('./routes/offerRoutes');
 const { mongoURI, cookieKey } = require('./config/keys');
@@ -13,6 +14,7 @@ mongoose.connect(mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
