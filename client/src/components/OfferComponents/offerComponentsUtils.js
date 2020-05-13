c46327f160c135d580dd9691cb520481d7aa7478
m-ossolinski <mateusz.ossolinski@gmail.com>
2020-05-13 20:35:54 +0200

    added new textareas to new offer form

---
+++ client/src/components/OfferComponents/offerComponentsUtils.js
@@ -0,0 +1,49 @@
+export const newOfferFormConfig = [
+  {
+    placeholder: "Nazwa stanowiska",
+    name: "title",
+    options: {
+      required: true,
+      min: 5,
+    },
+    errorMsg: "Nazwa stanowiska jest wymagana.",
+  },
+  {
+    placeholder: "Nazwa firmy",
+    name: "company",
+    options: {
+      required: true,
+      min: 3,
+    },
+    errorMsg: "Nazwa firmy jest wymagana.",
+  },
+  {
+    placeholder: "Opis stanowiska",
+    name: "description",
+    options: {
+      required: true,
+      min: 8,
+    },
+    errorMsg: "Opis stanowiska jest wymagany.",
+    type: "textarea",
+  },
+  {
+    placeholder: "Opis firmy",
+    name: "companyDescription",
+    options: {
+      min: 8,
+    },
+    errorMsg: "Opis firmy jest wymagany.",
+    type: "textarea",
+  },
+  {
+    placeholder: "Wymagania",
+    name: "requirements",
+    options: {
+      required: true,
+      min: 8,
+    },
+    errorMsg: "Pole jest wymagane.",
+    type: "textarea",
+  },
+];
--- client/src/components/pages/NewOfferPage/NewOfferPage.js
+++ client/src/components/pages/NewOfferPage/NewOfferPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppHeader, AppLayout } from "components/containers/AppLayout";
 import { AppNavigation } from "components/pages/AppNavigation";
-import { NewOfferForm } from "components/OfferComponents";
+import NewOfferForm from "components/OfferComponents/NewOfferForm";
 
 export const NewOfferPage = () => {
   return (
