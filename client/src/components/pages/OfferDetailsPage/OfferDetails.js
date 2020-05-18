5802b2632b7d0e8790f694b33dce2391c91d8b3b
m-ossolinski <mateusz.ossolinski@gmail.com>
2020-05-18 20:46:28 +0200

    added styles for offer details page

--- client/src/components/pages/OfferDetailsPage/OfferDetails.js
+++ client/src/components/pages/OfferDetailsPage/OfferDetails.js
@@ -3,23 +3,57 @@ import { useSelector } from "react-redux";
 
 export const OfferDetails = () => {
   const {
-    offerDetails: { title, description, company },
+    offerDetails: {
+      title,
+      description,
+      company,
+      jobLocalization,
+      requirements,
+      companyDescription,
+    },
     offerDetailsLoading,
+    offerDetails,
   } = useSelector((state) => state.offerDetails);
+
+  console.log(offerDetails, "offerDetails");
 
   return (
     <div>
       {offerDetailsLoading ? (
         <div>loading...</div>
       ) : (
-        <div>
-          <span>{title}</span>
-          <span>{description}</span>
-          <span>{company}</span>
+        <div className="offer-details">
+          <div className="offer-details__header">
+            <span>
+              <h2 className="offer-details__header-title">{title}</h2>
+              <p>{company}</p>
+            </span>
+            <span className="offer-details__header-localization">
+              <p>{jobLocalization}</p>
+            </span>
+          </div>
+          <div className="offer-details__info">
+            <span>
+              <p className="offer-details__info-title">Opis:</p>
+              <pre className="offer-details__info-description">
+                {description}
+              </pre>
+            </span>
+            <span>
+              <p className="offer-details__info-title">Wymagania:</p>
+              <pre className="offer-details__info-requirements">
+                {requirements}
+              </pre>
+            </span>
+            <span>
+              <p className="offer-details__info-title">O firmie:</p>
+              <pre className="offer-details__info-company-description">
+                {companyDescription}
+              </pre>
+            </span>
+          </div>
         </div>
       )}
-      offerdetails
-      <span> qweqwe</span>
     </div>
   );
 };
--- client/src/components/pages/OfferDetailsPage/OfferDetailsPage.js
+++ client/src/components/pages/OfferDetailsPage/OfferDetailsPage.js
@@ -2,11 +2,11 @@ import React from "react";
 import { AppHeader, AppLayout } from "components/containers/AppLayout";
 import { AppNavigation } from "components/containers/AppNavigation";
 import { OfferDetails } from "./OfferDetails";
+import "./offerDetails.scss";
 
 export const OfferDetailsPage = () => {
   return (
     <AppLayout appHeader={() => <AppHeader appNavigation={AppNavigation} />}>
-      offer details page
       <OfferDetails />
     </AppLayout>
   );
